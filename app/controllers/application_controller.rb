9f851598347835097ee06cfbcb7f23dde4612b6a
LE <feelgoood98@gmail.com>
2022-08-26 18:27:58 +0700

    Add the hello_App

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+def hello
+	render html: "Hello, world!"
+	#render html: "Dũng đẹp trai!"
+end
+def goodbye
+	render html: "Goodbye, world!"
+end
+
+end
